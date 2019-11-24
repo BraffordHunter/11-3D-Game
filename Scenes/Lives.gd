7ccdc5dafd37003f36b80b23e1f3e49bab7632c0
BraffordHunter <54556311+BraffordHunter@users.noreply.github.com>
2019-11-24 18:43:43 -0500

    Continued Working

---
+++ Scenes/Lives.gd
@@ -0,0 +1,26 @@
+extends Label
+
+#func _ready():
+	#update_lives()
+
+#func update_lives():
+#text = "Lives: " + str(get_parent().lives)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+#func _input(event):
+ #if event is InputEventMouseButton and event.pressed:
+ # if not get_parent().has_node("Player"):
+#   var Player = new_Player.instance(Player)
+#   Player.position = position - Vector2(0, 32)
+#   Player.name = "Player"
+#   Player.linear_velocity = Vector2(200, -200)
+#   get_parent().add_child(Player)
